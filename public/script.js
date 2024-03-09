5f59e058c3da36cc65c9226a908ed9d8020c8cc6
Kosilix <159823633+Kosilix@users.noreply.github.com>
2024-03-09 07:08:58 -0800

    Application data can be sent to database.

--- public/script.js
+++ public/script.js
@@ -1,5 +1,10 @@
-// Server side bullshit
-// const fs = require('fs'); 
+document.addEventListener("DOMContentLoaded", event => {
+
+  const app = firebase.app();
+  
+  const db = firebase.firestore();
+
+});
 
 let userValues = []
 let usersName = ''
@@ -8,28 +13,17 @@ function collectData(formClass){
   return document.querySelector(formClass).value; 
 }
 
-function saveData(Event){
-  Event.preventDefault()
-  for(let i = 0; i <= 8; i++){
-    userInput = collectData(`.js-${i}-form`);
-   
-    if (userInput == ''){
-      userValues[i] = 'N/A'
-    } else if(i === 0){
-      usersName = userInput
-      userValues[i] = userInput;
-      console.log(userValues);
-    } else {
-      userValues[i] = userInput;
-      console.log(userValues);
-    }
-
-  }
-
-  // saveArrayToFile(`${usersName}-Application`, userValues);
+function sendApplication(){
+  const db = firebase.firestore();
+  db.collection("Applications").doc(`${document.querySelector(".js-0-form").value}-${document.querySelector(".js-1-form").value}`).set({
+    firstName: `${document.querySelector('.js-0-form').value}`,
+    lastName: `${document.querySelector('.js-1-form').value}`,
+    email: `${document.querySelector('.js-2-form').value}`,
+    website: `${document.querySelector('.js-3-form').value}`,
+    headshot: `${document.querySelector('.js-4-form').value}`,
+    socials: `${document.querySelector('.js-5-form').value}`,
+    title: `${document.querySelector('.js-6-form').value}`,
+    story: `${document.querySelector('.js-7-form').value}`,
+    extraStory: `${document.querySelector('.js-8-form').value}`
+  })
 }
-
-// Server side bullshit
-// function saveArrayToFile(arrayName, array) {
-//   fs.writeFileSync(`/Applications/${arrayName}`, `${JSON.stringify(array)}`);
-// }
