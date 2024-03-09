ab5060bbdb76d6be2cebb1b2ffb0f07b25640bd5
Kosilix <159823633+Kosilix@users.noreply.github.com>
2024-03-09 08:28:39 -0800

    Image upload will now function.

--- public/script.js
+++ public/script.js
@@ -15,12 +15,19 @@ function collectData(formClass){
 
 function sendApplication(){
   const db = firebase.firestore();
+  const storageRef = firebase.storage().ref();
+  let imageRef = storageRef.child(`${document.querySelector('.js-0-form').value}-${document.querySelector('.js-1-form').value}.png`);
+  let imagePathRef = storageRef.child(`Photos/${document.querySelector('.js-0-form').value}-${document.querySelector('.js-1-form').value}.png`);
+
+  imagePathRef.put(new File([document.getElementById('head-shot').files[0]], imageRef)).then((snapshot) => {
+    console.log('Uploaded a blob or file!');
+  });
+
   db.collection("Applications").doc(`${document.querySelector(".js-0-form").value}-${document.querySelector(".js-1-form").value}`).set({
     firstName: `${document.querySelector('.js-0-form').value}`,
     lastName: `${document.querySelector('.js-1-form').value}`,
     email: `${document.querySelector('.js-2-form').value}`,
     website: `${document.querySelector('.js-3-form').value}`,
-    headshot: `${document.querySelector('.js-4-form').value}`,
     socials: `${document.querySelector('.js-5-form').value}`,
     title: `${document.querySelector('.js-6-form').value}`,
     story: `${document.querySelector('.js-7-form').value}`,
